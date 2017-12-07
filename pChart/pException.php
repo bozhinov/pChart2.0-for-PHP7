42b83c0b61d92b3e0593c799837468241ead3174
Momchil Bozhinov <momchil@bojinov.info>
2017-12-07 23:54:50 +0200

    * fixed identations

--- pChart/pException.php
+++ pChart/pException.php
@@ -1,44 +1,56 @@
 <?php
 
+/*
+pException - pChart core class
+
+Version     : 0.1
+Made by     : Created by Momchil Bozhinov
+Last Update : 27/11/2017
+
+This file can be distributed under the license you can find at:
+http://www.pchart.net/license
+
+*/
+
 namespace pChart;
 
 class pException extends \Exception
 {
 
-    public static function InvalidDimentions($text)
-    {
-        return new static(sprintf('pChart: %s', $text));
-    }
+	public static function InvalidDimentions($text)
+	{
+		return new static(sprintf('pChart: %s', $text));
+	}
 
-    public static function InvalidImageType($text)
-    {
-        return new static(sprintf('pChart: %s', $text));
-    }
-	
+	public static function InvalidImageType($text)
+	{
+		return new static(sprintf('pChart: %s', $text));
+	}
+
 	public static function InvalidInput($text)
-    {
-        return new static(sprintf('pChart: %s', $text));
-    }
-	
+	{
+		return new static(sprintf('pChart: %s', $text));
+	}
+
 	public static function InvalidResourcePath($text)
-    {
-        return new static(sprintf('pChart: %s', $text));
-    }
-	
+	{
+		return new static(sprintf('pChart: %s', $text));
+	}
+
 	public static function ImageMapInvalidID($text)
-    {
-        return new static(sprintf('pChart: %s', $text));
-    }
+	{
+		return new static(sprintf('pChart: %s', $text));
+	}
 
 	public static function ImageMapSessionNotStarted()
-    {
-        return new static('pChart: Yon need to start session before you can use the session storage');
-    }
-	
+	{
+		return new static('pChart: Yon need to start session before you can use the session storage');
+	}
+
 	public static function ImageMapInvalidSerieName($text)
-    {
+	{
 		return new static(sprintf('pChart: The serie name "%s" was not found in the dataset', $text));
-    }
+	}
 }
 
 ?>
