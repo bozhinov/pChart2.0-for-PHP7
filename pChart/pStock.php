1cf2ee15bf5ea44619bb7e3a5caee3d5a89fb79c
Momchil Bozhinov <momchil@bojinov.info>
2017-12-20 10:11:56 +0200

    * cleanup pStock

--- pChart/pStock.php
+++ pChart/pStock.php
@@ -80,8 +80,12 @@ class pStock
 		extract($Format);
 		
 		/* Data Processing */
-		$Data = $this->myPicture->myData->Data;
-		$Palette = $this->myPicture->myData->Palette;
+		$Data = $this->myPicture->myData->Data["Series"];
+		$Orientation = $this->myPicture->myData->Data["Orientation"];
+		
+		if (!isset($Data[$SerieOpen]) || !isset($Data[$SerieClose]) || !isset($Data[$SerieMin]) || !isset($Data[$SerieMax])) {
+			throw pException::StockMissingSerieException();
+		}
 		
 		if ($BoxUpSurrounding != NULL) {
 			$BoxUpBorderR = $BoxUpR + $BoxUpSurrounding;
@@ -95,37 +99,35 @@ class pStock
 			$BoxDownBorderB = $BoxDownB + $BoxDownSurrounding;
 		}
 
-		if ($LineWidth != 1) {
-			$LineOffset = $LineWidth / 2;
-		}
-
+		($LineWidth != 1) AND $LineOffset = $LineWidth / 2;
 		$BoxOffset = $BoxWidth / 2;
 
 		list($XMargin, $XDivs) = $this->myPicture->myData->scaleGetXSettings();
-		if (!isset($Data["Series"][$SerieOpen]) || !isset($Data["Series"][$SerieClose]) || !isset($Data["Series"][$SerieMin]) || !isset($Data["Series"][$SerieMax])) {
-			throw pException::StockMissingSerieException();
-		}
-
+		
 		$Plots = [];
-		foreach($Data["Series"][$SerieOpen]["Data"] as $Key => $Value) {
+		foreach($Data[$SerieOpen]["Data"] as $Key => $Value) {
 			$Point = [];
-			if (isset($Data["Series"][$SerieClose]["Data"][$Key]) || isset($Data["Series"][$SerieMin]["Data"][$Key]) || isset($Data["Series"][$SerieMax]["Data"][$Key])) {
-				$Point = array($Value,$Data["Series"][$SerieClose]["Data"][$Key],$Data["Series"][$SerieMin]["Data"][$Key],$Data["Series"][$SerieMax]["Data"][$Key]);
+			if (isset($Data[$SerieClose]["Data"][$Key]) || isset($Data[$SerieMin]["Data"][$Key]) || isset($Data[$SerieMax]["Data"][$Key])) {
+				$Point = array($Value,$Data[$SerieClose]["Data"][$Key], $Data[$SerieMin]["Data"][$Key], $Data[$SerieMax]["Data"][$Key]);
 			}
 
-			if ($SerieMedian != NULL && isset($Data["Series"][$SerieMedian]["Data"][$Key])) {
-				$Point[] = $Data["Series"][$SerieMedian]["Data"][$Key];
+			if ($SerieMedian != NULL && isset($Data[$SerieMedian]["Data"][$Key])) {
+				$Point[] = $Data[$SerieMedian]["Data"][$Key];
 			}
 
 			$Plots[] = $Point;
 		}
+		
+		if ($XDivs == 0){
+			$XStep = 0;
+		} else {
+			if ($Orientation == SCALE_POS_LEFTRIGHT) {
+				$XStep = ($this->myPicture->GraphAreaX2 - $this->myPicture->GraphAreaX1 - $XMargin * 2) / $XDivs;
+			} else {
+				$XStep = ($this->myPicture->GraphAreaY2 - $this->myPicture->GraphAreaY1 - $XMargin * 2) / $XDivs;
+			}
+		}
 
-		$AxisID = $Data["Series"][$SerieOpen]["Axis"];
-		$Mode = $Data["Axis"][$AxisID]["Display"];
-		$Format = $Data["Axis"][$AxisID]["Format"];
-		$Unit = $Data["Axis"][$AxisID]["Unit"];
-		$YZero = $this->myPicture->scaleComputeYSingle(0, ["AxisID" => $AxisID]);
-		$XStep = ($this->myPicture->GraphAreaX2 - $this->myPicture->GraphAreaX1 - $XMargin * 2) / $XDivs;
 		$X = $this->myPicture->GraphAreaX1 + $XMargin;
 		$Y = $this->myPicture->GraphAreaY1 + $XMargin;
 		$LineSettings = ["R" => $LineR,"G" => $LineG,"B" => $LineB,"Alpha" => $LineAlpha];
@@ -134,32 +136,29 @@ class pStock
 		$BoxDownSettings = ["R" => $BoxDownR,"G" => $BoxDownG,"B" => $BoxDownB,"Alpha" => $BoxDownAlpha,"BorderR" => $BoxDownBorderR,"BorderG" => $BoxDownBorderG,"BorderB" => $BoxDownBorderB,"BorderAlpha" => $BoxDownBorderAlpha];
 		$MedianSettings = ["R" => $MedianR,"G" => $MedianG,"B" => $MedianB,"Alpha" => $MedianAlpha];
 		
+		$ImageMapColor_1 = $this->myPicture->toHTMLColor($BoxUpR, $BoxUpG, $BoxUpB);
+		$ImageMapColor_2 = $this->myPicture->toHTMLColor($BoxDownR, $BoxDownG, $BoxDownB);
+		
 		foreach($Plots as $Key => $Points) {
 			
-			$PosArray = $this->myPicture->scaleComputeY($Points, ["AxisID" => $AxisID]);
-			$Values = "Open :" . $Data["Series"][$SerieOpen]["Data"][$Key] . "<br />Close : " . $Data["Series"][$SerieClose]["Data"][$Key] . "<br />Min : " . $Data["Series"][$SerieMin]["Data"][$Key] . "<br />Max : " . $Data["Series"][$SerieMax]["Data"][$Key] . "<br />";
+			$PosArray = $this->myPicture->scaleComputeY($Points, ["AxisID" => $Data[$SerieOpen]["Axis"]]);
 			
-			if ($SerieMedian != NULL) {
-				$Values = $Values . "Median : " . $Data["Series"][$SerieMedian]["Data"][$Key] . "<br />";
-			}
-
-			if ($PosArray[0] > $PosArray[1]) {
-				$ImageMapColor = $this->myPicture->toHTMLColor($BoxUpR, $BoxUpG, $BoxUpB);
-			} else {
-				$ImageMapColor = $this->myPicture->toHTMLColor($BoxDownR, $BoxDownG, $BoxDownB);
-			}
-
-			if ($Data["Orientation"] == SCALE_POS_LEFTRIGHT) {
+			if ($RecordImageMap) {
+				$Values = "Open :".$Data[$SerieOpen]["Data"][$Key]."<br />Close : ".$Data[$SerieClose]["Data"][$Key]."<br />Min : ".$Data[$SerieMin]["Data"][$Key]."<br />Max : ".$Data[$SerieMax]["Data"][$Key]."<br />";
 				
-				($YZero > $this->myPicture->GraphAreaY2 - 1) AND $YZero = $this->myPicture->GraphAreaY2 - 1;
-				($YZero < $this->myPicture->GraphAreaY1 + 1) AND $YZero = $this->myPicture->GraphAreaY1 + 1;
-
-				$XStep = ($XDivs == 0) ? 0 : ($this->myPicture->GraphAreaX2 - $this->myPicture->GraphAreaX1 - $XMargin * 2) / $XDivs;
-
-				if ($ShadowOnBoxesOnly) {
-					$RestoreShadow = $this->myPicture->Shadow;
-					$this->myPicture->Shadow = FALSE;
+				if ($SerieMedian != NULL) {
+					$Values = $Values."Median : ".$Data[$SerieMedian]["Data"][$Key]."<br />";
 				}
+
+				$ImageMapColor = ($PosArray[0] > $PosArray[1]) ? $ImageMapColor_1 : $ImageMapColor_2;
+			}
+				
+			if ($ShadowOnBoxesOnly) {
+				$RestoreShadow = $this->myPicture->Shadow;
+				$this->myPicture->Shadow = FALSE;
+			}
+
+			if ($Orientation == SCALE_POS_LEFTRIGHT) {
 
 				if ($LineWidth == 1) {
 					$this->myPicture->drawLine($X, $PosArray[2], $X, $PosArray[3], $LineSettings);
@@ -171,13 +170,13 @@ class pStock
 					$this->myPicture->drawLine($X - $ExtremityLength, $PosArray[2], $X + $ExtremityLength, $PosArray[2], $ExtremitySettings);
 					$this->myPicture->drawLine($X - $ExtremityLength, $PosArray[3], $X + $ExtremityLength, $PosArray[3], $ExtremitySettings);
 					if ($RecordImageMap) {
-						$this->myPicture->addToImageMap("RECT", floor($X - $ExtremityLength) . "," . floor($PosArray[2]) . "," . floor($X + $ExtremityLength) . "," . floor($PosArray[3]), $ImageMapColor, $ImageMapTitle, $Values);
+						$this->myPicture->addToImageMap("RECT", floor($X - $ExtremityLength).",".floor($PosArray[2]).",".floor($X + $ExtremityLength).",".floor($PosArray[3]), $ImageMapColor, $ImageMapTitle, $Values);
 					}
 				} else {
 					$this->myPicture->drawFilledRectangle($X - $ExtremityLength, $PosArray[2], $X + $ExtremityLength, $PosArray[2] - $ExtremityWidth, $ExtremitySettings);
 					$this->myPicture->drawFilledRectangle($X - $ExtremityLength, $PosArray[3], $X + $ExtremityLength, $PosArray[3] + $ExtremityWidth, $ExtremitySettings);
 					if ($RecordImageMap) {
-						$this->myPicture->addToImageMap("RECT", floor($X - $ExtremityLength) . "," . floor($PosArray[2] - $ExtremityWidth) . "," . floor($X + $ExtremityLength) . "," . floor($PosArray[3] + $ExtremityWidth), $ImageMapColor, $ImageMapTitle, $Values);
+						$this->myPicture->addToImageMap("RECT", floor($X - $ExtremityLength).",".floor($PosArray[2] - $ExtremityWidth).",".floor($X + $ExtremityLength).",".floor($PosArray[3] + $ExtremityWidth), $ImageMapColor, $ImageMapTitle, $Values);
 					}
 				}
 
@@ -189,35 +188,25 @@ class pStock
 
 				$X = $X + $XStep;
 				
-			} elseif ($Data["Orientation"] == SCALE_POS_TOPBOTTOM) {
+			} else { # if ($Orientation == SCALE_POS_TOPBOTTOM) { # Momchil: so I don't mess up the shadow
 				
-				($YZero > $this->myPicture->GraphAreaX2 - 1) AND $YZero = $this->myPicture->GraphAreaX2 - 1;
-				($YZero < $this->myPicture->GraphAreaX1 + 1) AND $YZero = $this->myPicture->GraphAreaX1 + 1;
-				
-				$XStep = ($XDivs == 0) ? 0 : ($this->myPicture->GraphAreaY2 - $this->myPicture->GraphAreaY1 - $XMargin * 2) / $XDivs;
-			
 				if ($LineWidth == 1) {
 					$this->myPicture->drawLine($PosArray[2], $Y, $PosArray[3], $Y, $LineSettings);
 				} else {
 					$this->myPicture->drawFilledRectangle($PosArray[2], $Y - $LineOffset, $PosArray[3], $Y + $LineOffset, $LineSettings);
 				}
 
-				if ($ShadowOnBoxesOnly) {
-					$RestoreShadow = $this->myPicture->Shadow;
-					$this->myPicture->Shadow = FALSE;
-				}
-
 				if ($ExtremityWidth == 1) {
 					$this->myPicture->drawLine($PosArray[2], $Y - $ExtremityLength, $PosArray[2], $Y + $ExtremityLength, $ExtremitySettings);
 					$this->myPicture->drawLine($PosArray[3], $Y - $ExtremityLength, $PosArray[3], $Y + $ExtremityLength, $ExtremitySettings);
 					if ($RecordImageMap) {
-						$this->myPicture->addToImageMap("RECT", floor($PosArray[2]) . "," . floor($Y - $ExtremityLength) . "," . floor($PosArray[3]) . "," . floor($Y + $ExtremityLength), $ImageMapColor, $ImageMapTitle, $Values);
+						$this->myPicture->addToImageMap("RECT", floor($PosArray[2]).",".floor($Y - $ExtremityLength).",".floor($PosArray[3]).",".floor($Y + $ExtremityLength), $ImageMapColor, $ImageMapTitle, $Values);
 					}
 				} else {
 					$this->myPicture->drawFilledRectangle($PosArray[2], $Y - $ExtremityLength, $PosArray[2] - $ExtremityWidth, $Y + $ExtremityLength, $ExtremitySettings);
 					$this->myPicture->drawFilledRectangle($PosArray[3], $Y - $ExtremityLength, $PosArray[3] + $ExtremityWidth, $Y + $ExtremityLength, $ExtremitySettings);
 					if ($RecordImageMap) {
-						$this->myPicture->addToImageMap("RECT", floor($PosArray[2] - $ExtremityWidth) . "," . floor($Y - $ExtremityLength) . "," . floor($PosArray[3] + $ExtremityWidth) . "," . floor($Y + $ExtremityLength), $ImageMapColor, $ImageMapTitle, $Values);
+						$this->myPicture->addToImageMap("RECT", floor($PosArray[2] - $ExtremityWidth).",".floor($Y - $ExtremityLength).",".floor($PosArray[3] + $ExtremityWidth).",".floor($Y + $ExtremityLength), $ImageMapColor, $ImageMapTitle, $Values);
 					}
 				}
 
