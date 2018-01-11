38f02327997b97e07d696727a766b6e58148e9f1
Momchil Bozhinov <momchil@bojinov.info>
2018-01-11 11:49:45 +0200

    * Fixed 1 example

--- examples/example.text.alignment.php
+++ examples/example.text.alignment.php
@@ -21,8 +21,8 @@ $myPicture->setFontProperties(array("FontName"=>"pChart/fonts/MankSans.ttf","Fon
 
 /* Draw the text box */
 $myPicture->setShadow(FALSE);
-$myPicture->drawFilledRectangle(141,77,393,126,["Color"=>new pColor(230,230,230,20)]);
-$myPicture->drawRectangle(141,77,393,126,["Color"=>new pColor(50,50,50)]);
+$myPicture->drawFilledRectangle(141,72,415,126,["Color"=>new pColor(230,230,230,20)]);
+$myPicture->drawRectangle(141,72,415,126,["Color"=>new pColor(50,50,50)]);
 
 /* Write the text */
 $myPicture->setShadow(TRUE,["X"=>1,"Y"=>1,"Color"=>new pColor(0,0,0,20)]);
@@ -32,36 +32,36 @@ $myPicture->drawText(144,125,"My text box",["Color"=>new pColor(201,230,40),"Fon
 $myPicture->setShadow(TRUE,["X"=>1,"Y"=>1,"Color"=>new pColor(0,0,0,20)]);
 $MyMarker = ["Color"=>new pColor(255,0,0),"BorderColor"=>new pColor(0,0,0),"Size"=>4];
 
-$myPicture->drawRectangleMarker(141,77,$MyMarker);
+$myPicture->drawRectangleMarker(141,72,$MyMarker);
 $myPicture->drawRectangleMarker(141,101,$MyMarker);
 $myPicture->drawRectangleMarker(141,126,$MyMarker);
 
-$myPicture->drawRectangleMarker(260,77,$MyMarker);
+$myPicture->drawRectangleMarker(260,72,$MyMarker);
 $myPicture->drawRectangleMarker(260,101,$MyMarker);
 $myPicture->drawRectangleMarker(260,126,$MyMarker);
 
-$myPicture->drawRectangleMarker(393,77,$MyMarker);
-$myPicture->drawRectangleMarker(393,101,$MyMarker);
-$myPicture->drawRectangleMarker(393,126,$MyMarker);
+$myPicture->drawRectangleMarker(415,72,$MyMarker);
+$myPicture->drawRectangleMarker(415,101,$MyMarker);
+$myPicture->drawRectangleMarker(415,126,$MyMarker);
 
 /* Change the font settings */
 $myPicture->setFontProperties(array("FontName"=>"pChart/fonts/MankSans.ttf","FontSize"=>7));
 $myPicture->setShadow(FALSE);
 
 /* Write the arrows */
-$myPicture->drawArrowLabel(139,75,"TEXT_ALIGN_TOPLEFT",["Length"=>20,"Angle"=>45,"RoundPos"=>TRUE]);
+$myPicture->drawArrowLabel(139,72,"TEXT_ALIGN_TOPLEFT",["Length"=>20,"Angle"=>45,"RoundPos"=>TRUE]);
 $myPicture->drawArrowLabel(139,101,"TEXT_ALIGN_MIDDLELEFT",["Length"=>20,"Angle"=>90,"RoundPos"=>TRUE]);
 $myPicture->drawArrowLabel(139,128,"TEXT_ALIGN_BOTTOMLEFT",["Length"=>20,"Angle"=>135,"RoundPos"=>TRUE]);
 
-$myPicture->drawArrowLabel(260,75,"TEXT_ALIGN_TOPMIDDLE",["Length"=>20,"Angle"=>45,"RoundPos"=>TRUE]);
+$myPicture->drawArrowLabel(260,72,"TEXT_ALIGN_TOPMIDDLE",["Length"=>20,"Angle"=>45,"RoundPos"=>TRUE]);
 $myPicture->drawArrowLabel(260,101,"TEXT_ALIGN_MIDDLEMIDDLE",["Length"=>90,"Angle"=>315,"RoundPos"=>TRUE]);
 $myPicture->drawArrowLabel(260,128,"TEXT_ALIGN_BOTTOMMIDDLE",["Length"=>20,"Angle"=>225,"RoundPos"=>TRUE]);
 
-$myPicture->drawArrowLabel(395,75,"TEXT_ALIGN_TOPRIGHT",["Length"=>20,"Angle"=>315,"RoundPos"=>TRUE]);
-$myPicture->drawArrowLabel(395,101,"TEXT_ALIGN_MIDDLERIGHT",["Length"=>20,"Angle"=>270,"RoundPos"=>TRUE]);
-$myPicture->drawArrowLabel(395,128,"TEXT_ALIGN_BOTTOMRIGHT",["Length"=>20,"Angle"=>225,"RoundPos"=>TRUE]);
+$myPicture->drawArrowLabel(415,72,"TEXT_ALIGN_TOPRIGHT",["Length"=>20,"Angle"=>315,"RoundPos"=>TRUE]);
+$myPicture->drawArrowLabel(415,101,"TEXT_ALIGN_MIDDLERIGHT",["Length"=>20,"Angle"=>270,"RoundPos"=>TRUE]);
+$myPicture->drawArrowLabel(415,128,"TEXT_ALIGN_BOTTOMRIGHT",["Length"=>20,"Angle"=>225,"RoundPos"=>TRUE]);
 
 /* Render the picture (choose the best way) */
-$myPicture->autoOutput("temp/example.spring.relations.png");
+$myPicture->autoOutput("temp/example.text.alignment.png");
 
 ?>
