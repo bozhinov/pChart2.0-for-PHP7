b1c70d1304192219a18a6b25669ebaba3d2be66e
Momchil Bozhinov <momchil@bojinov.info>
2018-01-19 15:57:21 +0200

    Examples page tweaked a bit more

--- examples/index.php
+++ examples/index.php
@@ -18,17 +18,17 @@ if (isset($_POST["View"]))
 <title>pChart 2.x - examples rendering</title>
 <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
 <style>
-body       { background-color: #F0F0F0; font-family: tahoma; font-size: 14px; height: 100%; overflow: auto;}
-table      { margin: 0px; padding: 0px; border: 0px; }
-tr         { margin: 0px; padding: 0px; border: 0px; }
-td         { font-family: tahoma; font-size: 11px; margin: 0px; padding: 0px; border: 0px; }
-div.folder { cursor: hand; cursor: pointer; }
-a.smallLinkGrey:link     { text-decoration: none; color: #6A6A6A; }
-a.smallLinkGrey:visited  { text-decoration: none; color: #6A6A6A; }
-a.smallLinkGrey:hover    { text-decoration: underline; color: #6A6A6A; }
-a.smallLinkBlack:link    { text-decoration: none; color: #000000; }
-a.smallLinkBlack:visited { text-decoration: none; color: #000000; }
-a.smallLinkBlack:hover   { text-decoration: underline; color: #000000; }
+	body       { background-color: #F0F0F0; font-family: tahoma; font-size: 14px; height: 100%; overflow: auto;}
+	table      { margin: 0px; padding: 0px; border: 0px; }
+	tr         { margin: 0px; padding: 0px; border: 0px; }
+	td         { font-family: tahoma; font-size: 11px; margin: 0px; padding: 0px; border: 0px; }
+	div.folder { cursor: hand; cursor: pointer; }
+	a.smallLinkGrey:link     { text-decoration: none; color: #6A6A6A; }
+	a.smallLinkGrey:visited  { text-decoration: none; color: #6A6A6A; }
+	a.smallLinkGrey:hover    { text-decoration: underline; color: #6A6A6A; }
+	a.smallLinkBlack:link    { text-decoration: none; color: #000000; }
+	a.smallLinkBlack:visited { text-decoration: none; color: #000000; }
+	a.smallLinkBlack:hover   { text-decoration: underline; color: #000000; }
 </style>
 <script src='imageMap/jquery-3.2.1.min.js' type="text/javascript"></script>
 <script>
@@ -64,10 +64,10 @@ a.smallLinkBlack:hover   { text-decoration: underline; color: #000000; }
 		document.getElementById(PictureName).style.fontWeight = "bold";
 		LastScript = PictureName;
 
-		ajaxRender(PictureName, PictureName);
+		ajaxRender(PictureName);
 	}
 
-	function ajaxRender(URL, SourceURL) // render the picture
+	function ajaxRender(URL) // render the picture
 	{
 		$.ajax({
 			url: URL,
@@ -77,7 +77,7 @@ a.smallLinkBlack:hover   { text-decoration: underline; color: #000000; }
 			},
 			complete: function(){
 				$("#render").html("<center><img src='" + URL + "' /></center>");
-				view(SourceURL); 
+				view(URL); 
 			}
 		});
 	}
@@ -103,11 +103,11 @@ a.smallLinkBlack:hover   { text-decoration: underline; color: #000000; }
 		<div style='font-size: 11px; padding: 2px; color: #FFFFFF; background-color: #666666; border-bottom: 3px solid #484848;'>&nbsp;Navigation</div>
 		<table style='padding: 1px; background-color: #E0E0E0; border: 1px solid #D0D0D0; border-top: 1px solid #FFFFFF;'>
 			<tr>
-			 <td width=16><img src='resources/application_view_tile.png' width=16 height=16 alt=''/></td>
+			 <td width=16><img src='resources/application_view_tile.png'/></td>
 			 <td width=100>&nbsp;<b>Examples</b></td>
-			 <td width=16><img src='resources/application_view_list.png' width=16 height=16 alt=''/></td>
+			 <td width=16><img src='resources/application_view_list.png'/></td>
 			 <td width=100>&nbsp;<a class=smallLinkGrey href='sandbox/'>Sandbox</a></td>
-			 <td width=16><img src='resources/application_view_list.png' width=16 height=16 alt=''/></td>
+			 <td width=16><img src='resources/application_view_list.png'/></td>
 			 <td width=100>&nbsp;<a class=smallLinkGrey href='imageMap/'>Image Map</a></td>
 			</tr>
 		</table>
@@ -161,7 +161,7 @@ echo <<<EOHTML
 	<div style='border: 3px solid #D0D0D0; border-top: 1px solid #FFFFFF; background-color: #FAFAFA; width: 220px; overflow: auto'>
 	<div style='padding: 1px; padding-bottom: 3px; color: #000000; background-color:#D0D0D0;'>
 	 <table><tr>
-	  <td><img src='resources/application_view_list.png' width=16 height=16 alt=''/></td>
+	  <td><img src='resources/application_view_list.png'/></td>
 	  <td>&nbsp;Examples folder contents</td>
 	 </tr></table>
 	</div>     
@@ -218,22 +218,22 @@ echo $_TREE_HTML;
 <td width=20></td>
 <td valign='top' style='padding-top: 5px; font-size: 12px;'>
 
-<table><tr><td><img src='resources/chart_bar.png' width=16 height=16 alt=''/></td><td>&nbsp;Rendering area</td></tr></table>
+<table><tr><td><img src='resources/chart_bar.png'/></td><td>&nbsp;Rendering area</td></tr></table>
 
 <div style='display:table-cell; padding: 10px; border: 2px solid #FFFFFF; vertical-align: middle; overflow: auto; background-image: url("resources/dash.png");'>
- <div style='font-size: 10px;' id=render>
-  <table><tr><td><img src='resources/accept.png' width=16 height=16 alt=""/></td><td>Click on an example to render it!</td></tr></table>
- </div>
+	<div style='font-size: 10px;' id="render">
+		<table><tr><td><img src='resources/accept.png'/></td><td>Click on an example to render it!</td></tr></table>
+	</div>
 </div>
 
 <br/><br/>
 
-<table><tr><td><img src='resources/application_view_list.png' width=16 height=16 alt=''/></td><td>&nbsp;Source area</td></tr></table>
+<table><tr><td><img src='resources/application_view_list.png'/></td><td>&nbsp;Source area</td></tr></table>
 
-<div style='display:table-cell; padding: 10px;  border: 2px solid #FFFFFF; vertical-align: middle; overflow: auto; background-image: url("resources/dash.png");'>
- <div style='font-size: 10px;' id=source style='width: 700px;'>
-  <table><tr><td><img src='resources/accept.png' width=16 height=16 alt=""/></td><td>Click on an example to get its source!</td></tr></table>
- </div>
+<div style='display:table-cell; padding: 10px; border: 2px solid #FFFFFF; vertical-align: middle; overflow: auto; background-image: url("resources/dash.png");'>
+	<div style='font-size: 10px;' id="source" style='width: 700px;'>
+		<table><tr><td><img src='resources/accept.png'/></td><td>Click on an example to get its source!</td></tr></table>
+	</div>
 </div>
 
 </td>
