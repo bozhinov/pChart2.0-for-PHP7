1f31ee63db58f826b57f06727478c6ac187bacb9
Momchil Bozhinov <momchil@bojinov.info>
2018-01-21 09:46:43 +0200

    * Examples: jQuery 3.2.1 => 3.3.1

--- examples/imageMap/index.php
+++ examples/imageMap/index.php
@@ -35,7 +35,7 @@ $Version = strip_tags($Values[1]);
 <meta http-equiv="expires" content="0" />
 <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
 <meta http-equiv="pragma" content="no-cache" />
-<script src='../resources/jquery-3.2.1.min.js' type="text/javascript"></script>
+<script src='../resources/jquery-3.3.1.min.js' type="text/javascript"></script>
 <script src='imagemap.js' type="text/javascript"></script>
 <script>
 $(document).ready(function() {
@@ -83,11 +83,11 @@ $(document).ready(function() {
 			<div style='font-size: 11px; padding: 2px; color: #FFFFFF; background-color: #666666; border-bottom: 3px solid #484848;'>&nbsp;Navigation</div>
 			<table style='padding: 1px; background-color: #E0E0E0; border: 1px solid #D0D0D0; border-top: 1px solid #FFFFFF;'>
 				<tr>
-					<td width=16><img src='../resources/application_view_tile.png' width=16 height=16 alt=''/></td>
+					<td width=16><img src='../resources/application_view_tile.png' /></td>
 					<td width=100>&nbsp;<a class=smallLinkGrey href='../'>Examples</a></td>
-					<td width=16><img src='../resources/application_view_list.png' width=16 height=16 alt=''/></td>
+					<td width=16><img src='../resources/application_view_list.png' /></td>
 					<td width=100>&nbsp;<a class=smallLinkGrey href='../sandbox/'>Sandbox</a></td>
-					<td width=16><img src='../resources/application_view_list.png' width=16 height=16 alt=''/></td>
+					<td width=16><img src='../resources/application_view_list.png' /></td>
 					<td width=100>&nbsp;<b>Image Map</b></td>
 				</tr>
 			</table>
@@ -106,7 +106,7 @@ $(document).ready(function() {
 				<div style='font-size: 11px; padding: 2px; color: #FFFFFF; background-color: #666666; border-bottom: 3px solid #484848; width: 222px;'>&nbsp;Release <?php echo $Version; ?></div>
 				<div style='border: 3px solid #D0D0D0; border-top: 1px solid #FFFFFF; background-color: #FAFAFA; width: 220px; overflow: auto'>
 					<div style='padding: 1px; padding-bottom: 3px; color: #000000; background-color:#D0D0D0;'>
-						<table><tr><td><img src='../resources/application_view_list.png' width=16 height=16 alt=''/></td><td>&nbsp;Examples folder contents</td></tr></table>
+						<table><tr><td><img src='../resources/application_view_list.png' /></td><td>&nbsp;Examples folder contents</td></tr></table>
 					</div>
 <?php
 
@@ -128,8 +128,8 @@ $(document).ready(function() {
 
 		echo "<table noborder cellpadding=0 cellspacing=0>\r\n";
 		echo " <tr valign=middle>\r\n";
-		echo "  <td><img src='".$Icon."' width=16 height=20 alt=''/></td>\r\n";
-		echo "  <td><img src='../resources/application_view_tile.png' width=16 height=16 alt=''/></td>\r\n";
+		echo "  <td><img src='".$Icon."' /></td>\r\n";
+		echo "  <td><img src='../resources/application_view_tile.png' /></td>\r\n";
 		echo "  <td><div class=folder id=".chr(34)."Hover-".$Element.chr(34).">&nbsp;".$Element."</div></td>\r\n";
 		echo " </tr>\r\n";
 		echo "</table>\r\n";
@@ -143,20 +143,20 @@ $(document).ready(function() {
 	<td width=20></td>
 	<td valign='top' style='padding-top: 5px; font-size: 12px;'>
 
-		<table><tr><td><img src='../resources/chart_bar.png' width=16 height=16 alt=''/></td><td>&nbsp;Rendering area</td></tr></table>
+		<table><tr><td><img src='../resources/chart_bar.png' /></td><td>&nbsp;Rendering area</td></tr></table>
 
 		<div style='display:table-cell; padding: 10px; border: 2px solid #FFFFFF; vertical-align: middle; overflow: auto; background-image: url("../resources/dash.png");'>
-			<div style='font-size: 10px;' id=render>
-				<table><tr><td><img src='../resources/accept.png' width=16 height=16 alt=""/></td><td>Click on an example to render it!</td></tr></table>
+			<div style='font-size: 10px;' id="render">
+				<table><tr><td><img src='../resources/accept.png' /></td><td>Click on an example to render it!</td></tr></table>
 			</div>
 		</div>
 
 		<br/><br/>
 
-		<table><tr><td><img src='../resources/application_view_list.png' width=16 height=16 alt=''/></td><td>&nbsp;HTML Source area</td></tr></table>
+		<table><tr><td><img src='../resources/application_view_list.png' /></td><td>&nbsp;HTML Source area</td></tr></table>
 
 		<div style='display:table-cell; padding: 10px;  border: 2px solid #FFFFFF; vertical-align: middle; overflow: auto; background-image: url("../resources/dash.png");'>
-			<div id=htmlsource style='width: 700px; font-size: 13px; font-family: Lucida Console'>
+			<div id="htmlsource" style='width: 700px; font-size: 13px; font-family: Lucida Console'>
 				&lt;html&gt;<br/>
 				&lt;head&gt;<br/>
 				&nbsp;&nbsp; &lt;style&gt;<br/>
@@ -164,7 +164,7 @@ $(document).ready(function() {
 				&nbsp;&nbsp; &lt;/style&gt;<br/>
 				&lt;/head&gt;<br/>
 				&lt;body&gt;<br/>
-				&nbsp;&nbsp; &lt;script src="jquery-3.2.1.min.js" type="text/javascript"&gt;&lt;/script&gt;<br/>
+				&nbsp;&nbsp; &lt;script src="jquery-3.3.1.min.js" type="text/javascript"&gt;&lt;/script&gt;<br/>
 				&nbsp;&nbsp; &lt;script src="imagemap.js" type="text/javascript"&gt;&lt;/script&gt;<br/>
 				&nbsp;&nbsp;  &lt;img src="draw.php" id="testPicture" alt="" class="pChartPicture"/&gt;<br/>
 				&lt;/body&gt;<br/>
@@ -179,14 +179,14 @@ $(document).ready(function() {
 
 		<table>
 			<tr>
-				<td><img src='../resources/application_view_list.png' width=16 height=16 alt=''/></td>
+				<td><img src='../resources/application_view_list.png' /></td>
 				<td>&nbsp;PHP Source area</td>
 			</tr>
 		</table>
 
 		<div style='display:table-cell; padding: 10px;  border: 2px solid #FFFFFF; vertical-align: middle; overflow: auto; background-image: url("../resources/dash.png");'>
-			<div style='font-size: 10px;' id=source style='width: 700px;'>
-				<table><tr><td><img src='../resources/accept.png' width=16 height=16 alt=""/></td><td>Click on an example to get its source!</td></tr></table>
+			<div style='font-size: 10px;' id="source" style='width: 700px;'>
+				<table><tr><td><img src='../resources/accept.png' /></td><td>Click on an example to get its source!</td></tr></table>
 			</div>
 		</div>
 
