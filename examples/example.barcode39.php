abccf82d6ba59efd56ce430ba0948294e597c90a
Momchil Bozhinov <momchil@bojinov.info>
2019-09-25 12:31:15 +0300

    Update example.barcode39.php

--- examples/example.barcode39.php
+++ examples/example.barcode39.php
@@ -18,9 +18,6 @@ use Barcodes\{
 /* Create the pChart object */
 $myPicture = new pDraw(700,230);
 
-/* Draw the picture border */
-$myPicture->drawRectangle(0,0,699,229,["Color"=>new pColor(0)]);
-
 /* Write the title */
 $myPicture->setFontProperties(["FontName"=>"pChart/fonts/PressStart2P-Regular.ttf","FontSize"=>6]);
 $myPicture->drawText(10,15,"Barcode 39 - Add barcode to your pictures",["Color"=>new pColor(0)]);
@@ -34,7 +31,10 @@ $Generator->forPChart($myPicture, "pChart Rocks!", [], 50,50);
 
 /* Draw a rotated barcode */
 //$Barcode->myPicture->setFontProperties(["FontName"=>"pChart/fonts/Cairo-Regular.ttf","FontSize"=>12]);
-$Generator->forPChart($myPicture, "Turn me on", ["Angle"=>90], 550,10);
+$Generator->forPChart($myPicture, "Turn me on", ["Angle"=>90], 600,20);
+
+//$Barcode->myPicture->setFontProperties(["FontName"=>"pChart/fonts/Cairo-Regular.ttf","FontSize"=>12]);
+$Generator->forPChart($myPicture, "Turn me on", ["Angle"=>350], 300,20);
 
 /* Render the picture (choose the best way) */
 $myPicture->autoOutput("temp/example.barcode39.png");
