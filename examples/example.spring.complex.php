cc10eb9e3338b4cad8c340645a15b6156efdd631
Momchil Bozhinov <momchil@bojinov.info>
2019-10-30 09:35:45 +0200

    * fixed 1 example
    1 in 20 threw invalid dimentions

--- examples/example.spring.complex.php
+++ examples/example.spring.complex.php
@@ -11,27 +11,27 @@ use pChart\{
 };
 
 /* Create the pChart object */
-$myPicture = new pDraw(600,600);
+$myPicture = new pDraw(700,700);
 
 /* Draw the background */
-$myPicture->drawFilledRectangle(0,0,600,600,["Color"=>new pColor(170,183,87), "Dash"=>TRUE, "DashColor"=>new pColor(190,203,107)]);
+$myPicture->drawFilledRectangle(0,0,700,700,["Color"=>new pColor(170,183,87), "Dash"=>TRUE, "DashColor"=>new pColor(190,203,107)]);
 
 /* Overlay with a gradient */
-$myPicture->drawGradientArea(0,0,600,600, DIRECTION_VERTICAL,["StartColor"=>new pColor(219,231,139,50),"EndColor"=>new pColor(1,138,68,50)]);
-$myPicture->drawGradientArea(0,0,600,20,  DIRECTION_VERTICAL,["StartColor"=>new pColor(0,0,0,80),"EndColor"=>new pColor(50,50,50,80)]);
+$myPicture->drawGradientArea(0,0,700,700, DIRECTION_VERTICAL,["StartColor"=>new pColor(219,231,139,50),"EndColor"=>new pColor(1,138,68,50)]);
+$myPicture->drawGradientArea(0,0,700,20,  DIRECTION_VERTICAL,["StartColor"=>new pColor(0,0,0,80),"EndColor"=>new pColor(50,50,50,80)]);
 
 /* Add a border to the picture */
-$myPicture->drawRectangle(0,0,599,599,["Color"=>new pColor(0,0,0)]);
+$myPicture->drawRectangle(0,0,699,699,["Color"=>new pColor(0,0,0)]);
 
 /* Write the picture title */ 
-$myPicture->setFontProperties(array("FontName"=>"fonts/PressStart2P-Regular.ttf","FontSize"=>6));
+$myPicture->setFontProperties(["FontName"=>"fonts/PressStart2P-Regular.ttf","FontSize"=>6]);
 $myPicture->drawText(10,15,"pSpring - Draw spring charts",["Color"=>new pColor(255)]);
 
 /* Set the graph area boundaries*/ 
-$myPicture->setGraphArea(20,20,580,580);
+$myPicture->setGraphArea(50,50,580,580);
 
 /* Set the default font properties */ 
-$myPicture->setFontProperties(array("FontName"=>"fonts/Cairo-Regular.ttf","FontSize"=>9,"Color"=>new pColor(80)));
+$myPicture->setFontProperties(["FontName"=>"fonts/Cairo-Regular.ttf","FontSize"=>9,"Color"=>new pColor(80)]);
 
 /* Enable shadow computing */ 
 $myPicture->setShadow(TRUE,["X"=>2,"Y"=>2,"Color"=>new pColor(0,0,0,10)]);
