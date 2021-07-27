4d8b768d00534d5a7d24460532dec9f48ff2e616
Momchil Bozhinov <momchil@bojinov.info>
2021-07-27 20:03:45 +0300

    * Do not carry 3rd party js

--- examples/index.php
+++ examples/index.php
@@ -14,7 +14,7 @@
 		background-image: url("resources/dash.png");
 	}
 </style>
-<script src='resources/jquery-3.4.1.min.js' type="text/javascript"></script>
+<script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script>
 	var LastOpened = null;
 	var LastScript = null;
--- examples/sandbox/index.php
+++ examples/sandbox/index.php
@@ -29,8 +29,8 @@ function listfonts($selected)
 	<title>Sandbox system</title>
 	<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
 	<link rel='stylesheet' type='text/css' href='style.css'/>
-	<script src='../resources/jquery-3.4.1.min.js' type="text/javascript"></script>
-	<script type='text/javascript' src='jquery.drawrpalette-min.js'></script>
+	<script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script type='text/javascript' src='https://rawrfl.es/jquery-drawrpalette/jquery.drawrpalette.js'></script>
 	<script type='text/javascript' src='functions.js'></script>
 </head>
 <body>
