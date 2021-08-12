cdc9ed12f39b91314ec363cda1fff03738f179ac
Momchil Bozhinov <momchil@bojinov.info>
2021-08-13 01:17:58 +0300

    Update Postnet.php

--- pChart/Barcodes/Linear/Postnet.php
+++ pChart/Barcodes/Linear/Postnet.php
@@ -4,68 +4,65 @@ namespace pChart\Barcodes\Linear;
 
 class Postnet {
 
-    public function encode($code, $opts) 
+	public function encode($code, $opts) 
 	{
-		$planet = ($opts['mode'] == "planet");
 		$orig = $code;
-        // bar lenght
-        if ($planet) {
-            $barlen = Array(
-                0 => Array(1, 1, 2, 2, 2),
-                1 => Array(2, 2, 2, 1, 1),
-                2 => Array(2, 2, 1, 2, 1),
-                3 => Array(2, 2, 1, 1, 2),
-                4 => Array(2, 1, 2, 2, 1),
-                5 => Array(2, 1, 2, 1, 2),
-                6 => Array(2, 1, 1, 2, 2),
-                7 => Array(1, 2, 2, 2, 1),
-                8 => Array(1, 2, 2, 1, 2),
-                9 => Array(1, 2, 1, 2, 2)
-            );
-        } else {
-            $barlen = Array(
-                0 => Array(2, 2, 1, 1, 1),
-                1 => Array(1, 1, 1, 2, 2),
-                2 => Array(1, 1, 2, 1, 2),
-                3 => Array(1, 1, 2, 2, 1),
-                4 => Array(1, 2, 1, 1, 2),
-                5 => Array(1, 2, 1, 2, 1),
-                6 => Array(1, 2, 2, 1, 1),
-                7 => Array(2, 1, 1, 1, 2),
-                8 => Array(2, 1, 1, 2, 1),
-                9 => Array(2, 1, 2, 1, 1)
-            );
-        }
+		// bar lenght
+		if ($opts['mode'] == "planet") {
+			$barlen = [
+				0 => [1, 1, 2, 2, 2],
+				1 => [2, 2, 2, 1, 1],
+				2 => [2, 2, 1, 2, 1],
+				3 => [2, 2, 1, 1, 2],
+				4 => [2, 1, 2, 2, 1],
+				5 => [2, 1, 2, 1, 2],
+				6 => [2, 1, 1, 2, 2],
+				7 => [1, 2, 2, 2, 1],
+				8 => [1, 2, 2, 1, 2],
+				9 => [1, 2, 1, 2, 2]
+			];
+		} else {
+			$barlen = [
+				0 => [2, 2, 1, 1, 1],
+				1 => [1, 1, 1, 2, 2],
+				2 => [1, 1, 2, 1, 2],
+				3 => [1, 1, 2, 2, 1],
+				4 => [1, 2, 1, 1, 2],
+				5 => [1, 2, 1, 2, 1],
+				6 => [1, 2, 2, 1, 1],
+				7 => [2, 1, 1, 1, 2],
+				8 => [2, 1, 1, 2, 1],
+				9 => [2, 1, 2, 1, 1]
+			];
+		}
 
-        $k = 0;
-        $code = str_replace('-', '', $code);
-        $code = str_replace(' ', '', $code);
-        $len = strlen($code);
-        // calculate checksum
-        $sum = 0;
-        for ($i = 0; $i < $len; ++$i) {
-            $sum += intval($code[$i]);
-        }
-        $chkd = ($sum % 10);
-        if ($chkd > 0) {
-            $chkd = (10 - $chkd);
-        }
-        $code .= $chkd;
-        $len = strlen($code);
-        // start bar
-		$block = [];
-		$block[] = [1, 1, 1, 2, 0];
-		$block[] = [0, 1, 1, 2, 0];
+		$code = str_replace('-', '', $code);
+		$code = str_replace(' ', '', $code);
 
-        for ($i = 0; $i < $len; ++$i) {
-            for ($j = 0; $j < 5; ++$j) {
-                $h = $barlen[$code[$i]][$j];
-                $p = floor(1 / $h);
+		// calculate checksum
+		$sum = array_sum(str_split($code));
+
+		$chkd = ($sum % 10);
+		if ($chkd > 0) {
+			$chkd = (10 - $chkd);
+		}
+		$code .= $chkd;
+		$len = strlen($code);
+		// start bar
+		$block = [
+			[1, 1, 1, 2, 0],
+			[0, 1, 1, 2, 0]
+		];
+
+		for ($i = 0; $i < $len; ++$i) {
+			for ($j = 0; $j < 5; ++$j) {
+				$h = $barlen[$code[$i]][$j];
+				$p = floor(1 / $h);
 				$block[] = [1, 1, 1, $h, $p];
 				$block[] = [0, 1, 1, 2, 0];
-            }
-        }
-        // end bar
+			}
+		}
+		// end bar
 		$block[] = [1, 1, 1, 2, 0];
 
 		return [
@@ -74,5 +71,5 @@ class Postnet {
 				'l' => [$orig]
 			]
 		];
-    }
+	}
 }
