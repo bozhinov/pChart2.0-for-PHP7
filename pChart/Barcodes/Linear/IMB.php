d34917e132889d5dc758c036428c86d37b81e53d
Momchil Bozhinov <momchil@bojinov.info>
2021-08-14 09:22:20 +0300

    * IMB: Speed up multiple encodings

--- pChart/Barcodes/Linear/IMB.php
+++ pChart/Barcodes/Linear/IMB.php
@@ -6,29 +6,39 @@ use pChart\pException;
 
 class IMB {
 
+	private $table2of13;
+	private $table5of13;
+
+	private $asc_chr = [
+		4, 0, 2, 6, 3, 5, 1, 9, 8, 7, 1, 2, 0, 6, 4, 8, 2, 9, 5, 3, 0, 1, 3, 7, 4, 6, 8, 9, 2, 0, 5, 1, 9, 4,
+		3, 8, 6, 7, 1, 2, 4, 3, 9, 5, 7, 8, 3, 0, 2, 1, 4, 0, 9, 1, 7, 0, 2, 4, 6, 3, 7, 1, 9, 5, 8
+	];
+
+	private $dsc_chr = [
+		7, 1, 9, 5, 8, 0, 2, 4, 6, 3, 5, 8, 9, 7, 3, 0, 6, 1, 7, 4, 6, 8, 9, 2, 5, 1, 7, 5, 4, 3, 8, 7, 6, 0, 2,
+		5, 4, 9, 3, 0, 1, 6, 8, 2, 0, 4, 5, 9, 6, 7, 5, 2, 6, 3, 8, 5, 1, 9, 8, 7, 4, 0, 2, 6, 3
+	];
+
+	private $asc_pos = [
+		3, 0, 8, 11, 1, 12, 8, 11, 10, 6, 4, 12, 2, 7, 9, 6, 7, 9, 2, 8, 4, 0, 12, 7, 10, 9, 0, 7, 10, 5, 7, 9, 6,
+		8, 2, 12, 1, 4, 2, 0, 1, 5, 4, 6, 12, 1, 0, 9, 4, 7, 5, 10, 2, 6, 9, 11, 2, 12, 6, 7, 5, 11, 0, 3, 2
+	];
+
+	private $dsc_pos = [
+		2, 10, 12, 5, 9, 1, 5, 4, 3, 9, 11, 5, 10, 1, 6, 3, 4, 1, 10, 0, 2, 11, 8, 6, 1, 12, 3, 8, 6, 4, 4, 11, 0,
+		6, 1, 9, 11, 5, 3, 7, 3, 10, 7, 11, 8, 2, 10, 3, 5, 8, 0, 3, 12, 11, 8, 4, 5, 1, 3, 0, 7, 12, 9, 8, 10
+	];
+
+	public function __construct()
+	{
+		// generate lookup tables
+		$this->table2of13 = $this->imb_tables(2, 78);
+		$this->table5of13 = $this->imb_tables(5, 1287);
+	}
+
 	public function encode(string $code, array $opts)
 	{
 		$orig = $code;
-
-		$asc_chr = [
-			4, 0, 2, 6, 3, 5, 1, 9, 8, 7, 1, 2, 0, 6, 4, 8, 2, 9, 5, 3, 0, 1, 3, 7, 4, 6, 8, 9, 2, 0, 5, 1, 9, 4,
-			3, 8, 6, 7, 1, 2, 4, 3, 9, 5, 7, 8, 3, 0, 2, 1, 4, 0, 9, 1, 7, 0, 2, 4, 6, 3, 7, 1, 9, 5, 8
-		];
-
-		$dsc_chr = [
-			7, 1, 9, 5, 8, 0, 2, 4, 6, 3, 5, 8, 9, 7, 3, 0, 6, 1, 7, 4, 6, 8, 9, 2, 5, 1, 7, 5, 4, 3, 8, 7, 6, 0, 2,
-			5, 4, 9, 3, 0, 1, 6, 8, 2, 0, 4, 5, 9, 6, 7, 5, 2, 6, 3, 8, 5, 1, 9, 8, 7, 4, 0, 2, 6, 3
-		];
-
-		$asc_pos = [
-			3, 0, 8, 11, 1, 12, 8, 11, 10, 6, 4, 12, 2, 7, 9, 6, 7, 9, 2, 8, 4, 0, 12, 7, 10, 9, 0, 7, 10, 5, 7, 9, 6,
-			8, 2, 12, 1, 4, 2, 0, 1, 5, 4, 6, 12, 1, 0, 9, 4, 7, 5, 10, 2, 6, 9, 11, 2, 12, 6, 7, 5, 11, 0, 3, 2
-		];
-
-		$dsc_pos = [
-			2, 10, 12, 5, 9, 1, 5, 4, 3, 9, 11, 5, 10, 1, 6, 3, 4, 1, 10, 0, 2, 11, 8, 6, 1, 12, 3, 8, 6, 4, 4, 11, 0,
-			6, 1, 9, 11, 5, 3, 7, 3, 10, 7, 11, 8, 2, 10, 3, 5, 8, 0, 3, 12, 11, 8, 4, 5, 1, 3, 0, 7, 12, 9, 8, 10
-		];
 
 		$code_arr = explode('-', $code);
 		$tracking_number = $code_arr[0];
@@ -77,17 +87,15 @@ class IMB {
 		if (($fcs >> 10) == 1) {
 			$codewords[9] += 659;
 		}
-		// generate lookup tables
-		$table2of13 = $this->imb_tables(2, 78);
-		$table5of13 = $this->imb_tables(5, 1287);
+
 		// convert codewords to characters
 		$characters = [];
 		$bitmask = 512;
 		foreach ($codewords as $k => $val) {
 			if ($val <= 1286) {
-				$chrcode = $table5of13[$val];
+				$chrcode = $this->table5of13[$val];
 			} else {
-				$chrcode = $table2of13[($val - 1287)];
+				$chrcode = $this->table2of13[($val - 1287)];
 			}
 			if (($fcs & $bitmask) > 0) {
 				// bitwise invert
@@ -100,25 +108,28 @@ class IMB {
 		// build bars
 		$block = [];
 		for ($i = 0; $i < 65; ++$i) {
-			$asc = (($characters[$asc_chr[$i]] & pow(2, $asc_pos[$i])) > 0);
-			$dsc = (($characters[$dsc_chr[$i]] & pow(2, $dsc_pos[$i])) > 0);
-			if ($asc AND $dsc) {
-				// full bar (F)
-				$p = 0;
-				$h = 3;
-			} elseif ($asc) {
-				// ascender (A)
-				$p = 0;
-				$h = 2;
-			} elseif ($dsc) {
-				// descender (D)
-				$p = 1;
-				$h = 2;
-			} else {
-				// tracker (T)
-				$p = 1;
-				$h = 1;
+
+			$asc = (bool)(($characters[$this->asc_chr[$i]] & pow(2, $this->asc_pos[$i])) > 0);
+			$dsc = (bool)(($characters[$this->dsc_chr[$i]] & pow(2, $this->dsc_pos[$i])) > 0);
+
+			switch (true) {
+				case ($asc AND $dsc): // full bar (F)
+					$p = 0;
+					$h = 3;
+					break;
+				case $asc: // ascender (A)
+					$p = 0;
+					$h = 2;
+					break;
+				case $dsc: // descender (D)
+					$p = 1;
+					$h = 2;
+					break;
+				default: // tracker (T)
+					$p = 1;
+					$h = 1;
 			}
+
 			$block[] = [1, 1, 1, $h, $p];
 			$block[] = [0, 1, 1, 2, 0];
 		}
