010e9450300305ceb1613e0d473e217a7b3fc1a6
Momchil Bozhinov <momchil@bojinov.info>
2021-08-15 18:40:19 +0300

    Update example.barcode.s25.php

--- examples/example.barcode.s25.php
+++ examples/example.barcode.s25.php
@@ -13,7 +13,7 @@ $myPicture = new pDraw(700,230);
 $myPicture->drawFilledRectangle(0,0,700,230,["Color"=>new pColor(179,217,91), "Dash"=>TRUE, "DashColor"=>new pColor(199,237,111)]);
 
 $opts = [
-	"mode" => "checksum",
+	"mode" => "Standard",
 	"scale" => 2,
 	"ratio" => 3,
 	"padding" => 10,
@@ -21,7 +21,7 @@ $opts = [
 ];
 
 # unable to verify. could not find app to scan it.
-$barcodes = new pBarcodes1D(BARCODES_ENGINE_S25, $myPicture);
+$barcodes = new pBarcodes1D(BARCODES_ENGINE_B2OF5, $myPicture);
 $barcodes->draw("51515", 0, 0, $opts);
 
 $myPicture->autoOutput("temp/example.barcodes.s25.png");
